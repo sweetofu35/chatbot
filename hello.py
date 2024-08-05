42ecda6ec65b26ed1eae65598a212189c054942c
sweetofu35 <62172639+sweetofu35@users.noreply.github.com>
2024-08-06 01:16:37 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,12 +15,9 @@ if not st.session_state.logged_in:
 
 if st.session_state.logged_in:
     st.title("환영합니다")
+    st.write(f'st.session_state.logged_in = {st.session_state.logged_in}')
 
     client = OpenAI(api_key=openai_api_key)
 
     if 'openai_api_key' not in st.session_state:
         st.session_state.openai_api_key = openai_api_key
-
-    if st.session_state.openai_api_key:
-        st.title("환영합니다")
-        st.write("hello")
