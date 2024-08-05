42f2919c267bb0a4ab1d3e5d31efbe61f22ed74f
sweetofu35 <62172639+sweetofu35@users.noreply.github.com>
2024-08-06 01:12:59 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,14 +4,14 @@ from openai import OpenAI
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
 
-if not st.session_state.logged_in:
-    st.title("코디 추천 앱 demo")
+    if not st.session_state.logged_in:
+        st.title("코디 추천 앱 demo")
 
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-    else:
-        st.session_state.logged_in = True
+        openai_api_key = st.text_input("OpenAI API Key", type="password")
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+        else:
+            st.session_state.logged_in = True
 
 if not st.session_state.logged_in:
     st.title("환영합니다")
